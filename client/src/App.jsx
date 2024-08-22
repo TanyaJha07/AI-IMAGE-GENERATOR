f1989ea0394f524d23696fa4ae51cddbc8b311cf
Tanya Jha <tanyajhaanni@gmail.com.com>
2024-08-22 11:03:09 +0530

    19:12 min

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,9 +9,8 @@ const App = () => {
       <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
       <Link to="/">
       <img src={logo} alt="logo" style={{ width: '100px', height: 'auto' }} />
-      
-
       </Link>
+      <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</Link> 
       </header>
     </BrowserRouter> //19.9
   )
